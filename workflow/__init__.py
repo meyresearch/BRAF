0cce7b7860def6f0319da2d322b3e759e7002400
Marco Mattia <61904349+marmatti@users.noreply.github.com>
2026-04-01 14:36:30 +0100

    Delete workflow directory

--- workflow/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Workflow package.
-
-This file exists so that modules under `workflow/` can reliably use
-package-relative imports (e.g., `from . import utilities`) when the
-workflow is imported from notebooks/scripts.
-"""
-
-
