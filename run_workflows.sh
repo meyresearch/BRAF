461727c27e5a077328b1db80a4b6f0d6176a6321
Marco Mattia <61904349+marmatti@users.noreply.github.com>
2026-04-01 14:37:37 +0100

    Delete run_workflows.sh

--- run_workflows.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-BASE="/home/marmatt/Documents/projects/BRAF/myWork/workflowJanuary2026"
-
-jupyter nbconvert --to notebook --execute "$BASE/Workflow1.ipynb"   --output "Workflow1.ran.ipynb"   --ExecutePreprocessor.timeout=-1
-jupyter nbconvert --to notebook --execute "$BASE/Workflow2.ipynb" --output "Workflow2.ran.ipynb" --ExecutePreprocessor.timeout=-1
-jupyter nbconvert --to notebook --execute "$BASE/Workflow3.ipynb" --output "Workflow3.ran.ipynb" --ExecutePreprocessor.timeout=-1
-jupyter nbconvert --to notebook --execute "$BASE/Workflow3.ipynb" --output "Workflow4.ran.ipynb" --ExecutePreprocessor.timeout=-1
